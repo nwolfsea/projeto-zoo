6f249797d376fe673ed792906eeca8f8febc1e41
natalia.palota <natalia.palota@zup.com.br>
2021-02-01 15:42:50 -0300

    Adicionar menu de interação com o usuário

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -3,11 +3,18 @@ package com.br.zup;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Map<String, List<String>> dicionarioAnimais = new HashMap<>();
+        boolean continuar = true;
+        while (continuar){
+            System.out.println("Digite 1 para cadastrar");
+            int resposta = new Scanner(System.in).nextInt();
+
+        }
     }
 }
