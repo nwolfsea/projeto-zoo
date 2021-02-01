62423b2f5aafe5ee637ff0da8be04a2fd2e62b12
natalia.palota <natalia.palota@zup.com.br>
2021-02-01 15:39:39 -0300

    Adicionar dicionário de animais de cada zoológico

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -1,8 +1,13 @@
 package com.br.zup;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Map<String, List<String>> dicionarioAnimais = new HashMap<>();
     }
 }
